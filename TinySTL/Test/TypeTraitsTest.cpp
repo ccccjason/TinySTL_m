69c8ba2aaa68362eda46bc6b75d37275934f33bf
gigi <gigi>
2016-04-11 15:28:23 +0800

    first commit

---
+++ TinySTL/Test/TypeTraitsTest.cpp
@@ -0,0 +1,17 @@
+#include "TypeTraitsTest.h"
+#include <cassert>
+
+namespace TinySTL
+{
+namespace TypeTraitsTest
+{
+void testCase1()
+{
+}
+
+void testAllCases()
+{
+    testCase1();
+}
+}
+}
---
+++ TinySTL/Test/TypeTraitsTest.h
@@ -0,0 +1,14 @@
+#ifndef _TYPE_TRAITS_TEST_H_
+#define _TYPE_TRAITS_TEST_H_
+
+#include "../TypeTraits.h"
+
+namespace TinySTL
+{
+namespace TypeTraitsTest
+{
+void testAllCases();
+}
+}
+
+#endif
